9c077e20c942c6e4154dfbdf49aec0b0c2e58a1b
Shalinnjuguna <47768723+Shalinnjuguna@users.noreply.github.com>
2019-04-15 17:26:52 +0300

    Add files via upload

---
+++ calculate.py
@@ -0,0 +1,31 @@
+def add(a,b):
+	answer=a+b
+	return answer
+
+def subtract(a,b):
+	answer=a-b
+	return answer
+
+def wholenumber(b,c):
+	answer=b//c
+	return answer
+
+def exponent(b,c):
+	answer=b**c 
+	return answer	
+
+def percentage(c,d):
+	answer=c%d
+	return answer
+
+def divide(c,d):
+	answer=c/d
+	return answer
+
+def modulus(c,d):
+	answer=c%d		
+	return answer
+
+def multiply(c,d):
+	answer=c*d
+	return answer
